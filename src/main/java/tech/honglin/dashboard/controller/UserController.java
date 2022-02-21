5af39cd6c33d63fb4faa8f826a2417155aa58047
Honglin <honglin.lu.sr@gmail.com>
2022-02-21 10:50:33 -0500

    add getall and delete

--- src/main/java/tech/honglin/dashboard/controller/UserController.java
+++ src/main/java/tech/honglin/dashboard/controller/UserController.java
@@ -3,28 +3,19 @@ package tech.honglin.dashboard.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import tech.honglin.dashboard.entity.User;
 import tech.honglin.dashboard.repository.UserRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
-
-    @GetMapping("/str")
-    String getStr() {
-        return "this is str";
-    }
-
-    @GetMapping("/w")
-    int error() {
-        throw new RuntimeException("this is exception");
-    }
 
     @PostMapping("/users")
     User createOrSaveUser(@RequestBody User newUser) {
@@ -36,6 +27,15 @@ public class UserController {
         return userRepository.findById(id).get();
     }
 
+    @GetMapping("/users")
+    Page<User> getUserList(@PageableDefault(value = 6) Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
 
+    @DeleteMapping("/users/{id}")
+    boolean deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+        return true;
+    }
 
 }
--- src/main/java/tech/honglin/dashboard/repository/UserRepository.java
+++ src/main/java/tech/honglin/dashboard/repository/UserRepository.java
@@ -1,15 +1,23 @@
 package tech.honglin.dashboard.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import tech.honglin.dashboard.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    Page<User> findAll(Pageable pageable);
 
     Optional<User> findByUsername(String name);
 
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    void deleteById(Long id);
 }
